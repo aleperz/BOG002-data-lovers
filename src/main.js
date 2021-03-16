843c8ef3d23f55e8fe1864c7f1a6fb38f0bc76e2
aleperz <alejandraperez.659@gmail.com>
2021-03-15 23:57:38 -0500

    Se extraen valores unicos de cada categoria para definir filtros mas viables

--- src/main.js
+++ src/main.js
@@ -1,6 +1,63 @@
-import { example } from './data.js';
+import { example, anotherExample } from './data.js';
 
+//Despliegue de subitems en barra lateral de filtrado
+let options = document.querySelectorAll(".options_filterContainer")
+options.forEach(item => {
+    item.addEventListener("click", () => {
+    item.querySelector('.chosen').toggleAttribute('hidden') 
+    })})
+
+//Extrayendo valor del checkbox
+/* let checks = document.querySelectorAll() */
 
 import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+/* console.log(example, anotherExample,  */
+
+//Mostrando los valores unicos de genero    
+    const gender = []    
+    data.results.forEach(item =>{
+    gender.push(item.gender)
+});
+let result = new Set(gender)
+console.log([...result])
+
+//Mostrando los valores unicos de status
+const status = []    
+data.results.forEach(item =>{
+status.push(item.status)
+});
+let uniqueStatus = new Set(status)
+console.log([...uniqueStatus])
+
+//Mostrando los valores unicos de species
+const species = []    
+data.results.forEach(item =>{
+species.push(item.species)
+});
+let uniqueSpecies = new Set(species)
+console.log([...uniqueSpecies])
+
+//Mostrando los valores unicos de type
+const type = []    
+data.results.forEach(item =>{
+    type.push(item.type)
+});
+let uniqueType = new Set(type)
+console.log([...uniqueType])
+
+//Mostrando los valores unicos de origin
+const origin = []    
+data.results.forEach(item =>{
+    origin.push(item.origin.name)
+});
+let uniqueOrigin = new Set(origin)
+console.log([...uniqueOrigin])
+
+//Mostrando los valores unicos de location
+const location = []    
+data.results.forEach(item =>{
+    location.push(item.location.name)
+});
+let uniqueLocation = new Set(location)
+console.log([...uniqueLocation])
